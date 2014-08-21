1b00d10af9edc102df815abc1d41e9d68e9941bf
ZZhiming <zhangzm05@gmail.com>
2014-08-20 23:16:42 -0500

    Update PDFLineRestore.java

--- PDFLineRestore.java
+++ PDFLineRestore.java
@@ -14,23 +14,7 @@ public class PDFLineRestorer {
 		File srcFile = new File(args[0]);
 		BufferedReader in = new BufferedReader( new FileReader(srcFile));
 		File outFile = new File(args[1]);
-		PrintWriter out = new PrintWriter(new FileWriter(outFile, true));
-		String inLine = "", outLine = "";
-		String[] splits;
-		while ((inLine = in.readLine())!=null) {
-			splits = inLine.trim().split(" ");
-			if (inLine.equals("")) {
-				if (outLine.endsWith(" "))
-					outLine = outLine.substring(0, outLine.length()-1);
-				outLine = outLine + "\n\n";
-				continue;
-			}
-			for (int i = 0; i< splits.length-1; i++)
-				outLine = outLine + splits[i] + " ";
-			String lastWord = splits[splits.length-1];
-			if (lastWord.endsWith("-"))
-				outLine = outLine + lastWord.substring(0, lastWord.length()-1);
-			else outLine = outLine + lastWord + " ";
+	
 		}
 		out.write(outLine);
 		in.close();
